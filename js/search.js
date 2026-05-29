c80120e59439d385f3a45bf5d466d078b919871a
sashaxodunov <sasha@xodunov.ru>
2026-05-29 21:58:39 +0300

    День1. Усложненное задание

--- js/search.js
+++ js/search.js
@@ -2,8 +2,13 @@ const search = function () {
   const input = document.querySelector('.search-block > input')
   const searchBtn = document.querySelector(".search-block > button")
 
-  input.addEventListener('input', (event) => {
-    console.dir(event.target.value);
+
+  // input.addEventListener('input', (event) => {
+  //   console.dir(event.target.value);
+  // })
+
+  searchBtn.addEventListener('click', () => {
+    console.dir(input.value);
   })
 }
 
